f08984ce8556a8e39bf473bf8e95211e50ac8cae
IvannaMo <ivanna.mo091@gmail.com>
2024-11-06 11:46:31 +0200

    Add Homework 2

--- Assets/Scripts/HexagonScript.cs
+++ Assets/Scripts/HexagonScript.cs
@@ -2,9 +2,39 @@ using UnityEngine;
 
 public class HexagonScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb2b;
+
     void Start()
     {
-        Debug.Log("Hello World!");   
+        rb2b = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        int value = 300;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb2b.AddForce(Vector2.up * value);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb2b.AddForce(Vector2.left * value);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb2b.AddForce(Vector2.right * value);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb2b.AddTorque(value);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            rb2b.angularVelocity = 0f;
+            rb2b.linearVelocity = Vector2.zero;
+        }
     }
 }
+
+
