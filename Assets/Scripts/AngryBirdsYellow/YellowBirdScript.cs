cce08a5bd4145071f392f335b8268693ad1077c0
IvannaMo <ivanna.mo091@gmail.com>
2024-11-22 01:58:03 +0200

    Add Homework 5

--- Assets/Scripts/AngryBirdsYellow/YellowBirdScript.cs
+++ Assets/Scripts/AngryBirdsYellow/YellowBirdScript.cs
@@ -4,18 +4,42 @@ public class YellowBirdScript : MonoBehaviour
 {
     [SerializeField]
     private Transform arrow;
+
     private Rigidbody2D rb2d;
+    private ForceScript forceScript;
+    private bool isDashAllowed;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        forceScript = GameObject.Find("ForceCanvasIndicator").GetComponent<ForceScript>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb2d.AddForce(850 * arrow.right);
+            float forceFactor = 1000.0f;
+
+            if (!isDashAllowed)
+            {
+                forceFactor = 1000.0f;
+            }
+            else
+            {
+                forceFactor *= 3.0f;
+                isDashAllowed = false;
+            }
+
+            if (forceScript != null)
+            {
+                forceFactor *= Time.timeScale * (forceScript.forceFactor + 0.5f);
+            }
+            else
+            {
+                Debug.LogError("forceScript NULL, used default");
+            }
+            rb2d.AddForce(forceFactor * arrow.right);
         }
     }
 }
